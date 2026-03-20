7119e791ec29d09748f962a0cdd91e3d61f8c559
iamdookie1 <toofast133@gmail.com>
2026-03-20 17:39:50 -0400

    Modify getIndex function to accept a seed parameter

--- api/Cum.js
+++ api/Cum.js
@@ -4,13 +4,14 @@ const data = [
   { msg: "{u} is getting lots of cum from {a}! <3", gif: "https://cdn.nsfwgify.com/66599/blonde-cumslut-scaled.webp" }
 ];
 
-function getIndex() {
+function getIndex(seed) {
+  if (seed !== undefined) return parseInt(seed) % data.length;
   return Math.floor(Date.now() / 10000) % data.length;
 }
 
 export default function handler(req, res) {
-  const { type, u, a } = req.query;
-  const index = getIndex();
+  const { type, u, a, i } = req.query;
+  const index = getIndex(i);
   if (type === "gif") {
     res.status(200).send(data[index].gif.trim());
   } else {
